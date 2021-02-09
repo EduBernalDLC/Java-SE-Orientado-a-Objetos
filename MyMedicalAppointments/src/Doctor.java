4abc74cf5869f10edf0902451ae0a99e3ca099df
Eduardo Bernal <eduardo._1210@hotmail.com>
2021-02-08 20:15:21 -0600

    Primera Clase :) #5

---
+++ MyMedicalAppointments/src/Doctor.java
@@ -0,0 +1,16 @@
+public class Doctor {
+    int id;
+    String name;
+    String speciality;
+
+    //Comportamientos
+    /**
+     * Método para imprimir el nombre del doctor
+     */
+    public void showName(){
+        System.out.println(name);
+    }//Fin método para imprimir el nombre del doctor
+
+
+
+}//Fin clase
---
+++ MyMedicalAppointments/src/Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Doctor myDoctor= new Doctor();
+        myDoctor.name= "Alejandro López";
+        myDoctor.showName();
+
+
+
+
+
+
+
+
+    }//Fin main
+
+}//Fin clase
