02004c831c618e684f15f6db7a9ba065d78b5f38
Eduardo Bernal <eduardo._1210@hotmail.com>
2021-03-03 15:44:37 -0600

    Clases Anónimas #28

--- MyMedicalAppointments/src/Main.java
+++ MyMedicalAppointments/src/Main.java
@@ -1,4 +1,5 @@
 import model.Doctor;
+import model.ISchedulable;
 import model.Patient;
 import model.User;
 
@@ -22,6 +23,24 @@ public class Main {
         User userPa= new Patient("Karla", "karla@karla.com");
         userPa.showDataUser();
 
+        User user1= new User("Oscar", "oscar@gmail.com") {
+            @Override
+            public void showDataUser() {
+                System.out.println("Supuesto Doctor\n");
+                System.out.println("Hospital: Cruz azul");
+                System.out.println("Departamento: Geriatria");
+            }//Fin comportamiento clase anónima
+        };//Fin clase anónima
+
+        user1.showDataUser();//Implementamos la clase anónima
+
+        ISchedulable iSchedulable= new ISchedulable() {
+            @Override
+            public void schedule(Date date, String time) {
+
+            }//Fin comportamiento de clase anónima
+        };//Fin clase anónima
+        
         /*
         for (model.Doctor.AvailableAppointment availableAppointment : myDoctor.availableAppointments){
             System.out.println(availableAppointment + " " + availableAppointment.getTime());
